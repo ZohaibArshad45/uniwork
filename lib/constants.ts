6caeb1a7110318575d3c4a25da888b1667dd55b4
Zohaib Arshad <zohaibfta@gmail.com>
2025-01-07 23:36:23 +0500

    Create Product Details Page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,8 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "UniWork";
-export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Your work, your way";
-export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "UniWork is a job search platform that connects job seekers with employers";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NxtAmzn'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An Amazon clone built with Next.js and MongoDB'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
