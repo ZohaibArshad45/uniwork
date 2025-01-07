11a4ea853674329d7ce135bc3e73c606adb3e035
Zohaib Arshad <zohaibfta@gmail.com>
2025-01-07 19:10:06 +0500

    Navbar , Footer, layout

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "UniWork";
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Your work, your way";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "UniWork is a job search platform that connects job seekers with employers";
