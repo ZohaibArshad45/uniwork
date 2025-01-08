bf1eb91fc2e1311c447189d4aeebcd981b24c9f5
Zohaib Arshad <zohaibfta@gmail.com>
2025-01-08 10:51:03 +0500

    Create Login User Page

--- types/index.ts
+++ types/index.ts
@@ -2,25 +2,32 @@ import {
     CartSchema,
     OrderItemSchema,
     ProductInputSchema,
-} from '@/lib/validator'
-import { z } from 'zod'
-
-export type IProductInput = z.infer<typeof ProductInputSchema>
-
-export type Data = {
+    UserInputSchema,
+    UserSignInSchema,
+  } from '@/lib/validator'
+  import { z } from 'zod'
+  
+  export type IProductInput = z.infer<typeof ProductInputSchema>
+  
+  export type Data = {
+    users: IUserInput[]
     products: IProductInput[]
     headerMenus: {
-        name: string
-        href: string
+      name: string
+      href: string
     }[]
     carousels: {
-        image: string
-        url: string
-        title: string
-        buttonCaption: string
-        isPublished: boolean
+      image: string
+      url: string
+      title: string
+      buttonCaption: string
+      isPublished: boolean
     }[]
-}
-
-export type OrderItem = z.infer<typeof OrderItemSchema>
-export type Cart = z.infer<typeof CartSchema>
+  }
+  
+  export type OrderItem = z.infer<typeof OrderItemSchema>
+  export type Cart = z.infer<typeof CartSchema>
+  
+  // user
+  export type IUserInput = z.infer<typeof UserInputSchema>
+  export type IUserSignIn = z.infer<typeof UserSignInSchema>
